e0d10e6b55aaf7d2e8e82a3c7f6cfe99f728936d
Julio Cesar Mello Boaroli <julio.boaroli@projuris.com.br>
2021-09-20 16:45:15 -0300

    Melhorias de UX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import wordList from './resources/words.json';
 
 const MAX_TYPED_KEYS = 30;
+const WORD_ANIMATION_INTERVAL = 200;
 
 const getWord = () => {
     const index = Math.floor(Math.random() * wordList.length)
@@ -22,9 +23,11 @@ const Word = ({ word, validKeys }) => {
     const matched = word.slice(0, validKeys.length)
     const remainder = word.slice(validKeys.length)
 
+    const matchedClass = (joinedKeys === word) ? 'matched completed': ''; 
+
     return (
         <>
-            <span className="matched">{matched}</span>
+            <span className={matchedClass}>{matched}</span>
             <span className="remainder">{remainder}</span>
         </>
     )
@@ -36,22 +39,32 @@ const App = () => {
     const [validKeys, setValidkeys] = useState([])
     const [completedWords, setCompletedWords] = useState([])
     const [word, setWord] = useState('')
+    const containerRef = useRef(null)
 
     // Para rodar ao inicializar
     useEffect(() => {
         setWord(getWord())
+        if (containerRef) containerRef.current.focus()
     }, [])
 
     useEffect(() => {
         const wordFromValidKeys = validKeys.join('').toLowerCase();
+        let timeout = null;
         if (word && word === wordFromValidKeys) {
-            let newWord = null
-            do {
-                newWord = getWord()
-            } while(completedWords.includes(newWord))
-            setWord(newWord)
-            setValidkeys([])
-            setCompletedWords((prev) => [...prev, word])
+            timeout = setTimeout(() => {
+                let newWord = null
+                do {
+                    newWord = getWord()
+                } while(completedWords.includes(newWord))
+                setWord(newWord)
+                setValidkeys([])
+                setCompletedWords((prev) => [...prev, word])
+            }, WORD_ANIMATION_INTERVAL)
+        }
+        
+        // Para limpar o timeout. Necessidade do React. Vide documentação do useEffect
+        return () => {
+            if (timeout) clearTimeout(timeout)
         }
     }, [word, validKeys, completedWords])
 
@@ -71,7 +84,7 @@ const App = () => {
 
     return (
         // Passado o tabIndex para o react entender o evento, já que div não é um elemento de input
-    <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
+    <div className="container" tabIndex="0" onKeyDown={handleKeyDown} ref={containerRef}>
         <div className="valid-keys">
             <Word word={word} validKeys={validKeys}/>
         </div>
